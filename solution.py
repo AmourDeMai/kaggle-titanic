50ff1056e676588a5c341c215e59e2a3054463c4
AmourDeMai <fengyh7@gmail.com>
2016-04-12 17:15:23 +0200

    reform the functions

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python
+#!/usr/bin/python
 
 import pandas as pd
 import numpy as np
@@ -12,9 +12,8 @@ def fill_missing_ages(data):
     """Fill missing age using RandomForestClassifier"""
 
     # Selete features for training
-    embarked = pd.get_dummies(data['Embarked'], prefix='Embarked')
-    age_data = pd.concat([data[['Age', 'SibSp', 'Parch', 'Fare', 'Pclass']],
-                                embarked], axis=1)
+    age_data = data[['Age', 'SibSp', 'Parch', 'Fare', 'Pclass']]
+
     # Divide passengers into with and without age
     with_age = age_data.loc[age_data['Age'].notnull()]
     without_age = age_data.loc[age_data['Age'].isnull()]
@@ -35,8 +34,8 @@ def fill_missing_ages(data):
 def set_cabin_type(data):
     """Set cabin into 2 types: with or without value """
 
-    data.loc[train_data['Cabin'].notnull(), 'Cabin'] = 'Yes'
-    data.loc[train_data['Cabin'].isnull(), 'Cabin'] = 'No'
+    data.loc[data['Cabin'].notnull(), 'Cabin'] = 'Yes'
+    data.loc[data['Cabin'].isnull(), 'Cabin'] = 'No'
 
     return data
 
@@ -44,10 +43,12 @@ def set_cabin_type(data):
 def extract_features(data):
     """Features extraction"""
 
-    # Set cain type
+    # Set cabin type
     data = set_cabin_type(data)
     # Fill Embarked missing data with S
-    train_data["Embarked"] = train_data["Embarked"].fillna("S")
+    data["Embarked"] = data["Embarked"].fillna("S")
+    # Fill Fare missing data with the median
+    data['Fare'] = data['Fare'].fillna(data['Fare'].median())
     # Fill missing value of ages
     data, alg = fill_missing_ages(data)
 
@@ -74,11 +75,11 @@ def extract_features(data):
     return features
 
 
-def train_model(train_data):
-    """Train a model """
+def train_model(data):
+    """Train a model"""
 
-    # Extract features from train data
-    features = extract_features(train_data)
+    # Extract train features
+    features = extract_features(data)
 
     # Initialize algorithm class
     alg = LogisticRegression()
@@ -91,16 +92,33 @@ def train_model(train_data):
         prediction = alg.predict(features.iloc[test, :])
         predictions.append(prediction)
 
-    return predictions, features
+    return predictions, alg
 
+
+def evaluation(data, classifier):
+    """Evaluate test data"""
+
+    # Extract test features
+    features = extract_features(data)
+    # Predict test data
+    prediction = classifier.predict(features)
+
+    return prediction
 
 # main
-# Load data
-train_data = pd.read_csv('data/train.csv')
-test_data = pd.read_csv('data/test.csv')
+if __name__ == '__main__':
 
-predictions, features = train_model(train_data)
+    # Load train data
+    train_data = pd.read_csv('data/train.csv')
+    # CV result, save trained classifier alg
+    predictions, alg = train_model(train_data)
+    predictions = np.concatenate(predictions, axis=0)
+    # Calculate CV accuracy
+    error_nb = sum(abs(predictions - pd.Series.as_matrix(train_data.Survived)))
+    accuracy = 1 - error_nb / train_data.shape[0]
 
-predictions = np.concatenate(predictions, axis=0)
-error_nb = sum(abs(predictions - pd.Series.as_matrix(train_data.Survived)))
-accuracy = 1 - error_nb / train_data.shape[0]
+    # Load test data
+    test_data = pd.read_csv('data/test.csv')
+    predictions_test = evaluation(test_data, alg)
+
+    print("accuracy for the training data is:", accuracy)
